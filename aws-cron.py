90e124e69a64854853bbc2e49e3f987343533bc7
Ted Mielczarek <ted@mielczarek.org>
2015-05-26 15:33:09 -0400

    fix aws-cron

--- aws-cron.py
+++ aws-cron.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import boto
 import gzip
 import io
+import logging
 import shutil
 
 from boto.s3.key import Key
@@ -29,15 +30,20 @@ def get_from_s3_compressed(bucket, keyname, filename):
         shutil.copyfileobj(g, f)
 
 def main():
+    logging.basicConfig(filename='scanpackages.log',
+                        level=logging.DEBUG)
+    log = logging.getLogger('aws-cron')
     conn = boto.connect_s3()
     bucket = conn.get_bucket('ubuntu-build-ids')
     bucket_location = bucket.get_location()
     if bucket_location:
         conn = boto.s3.connect_to_region(bucket_location)
         bucket = conn.get_bucket('ubuntu-build-ids')
-    print('Fetching ddebs.json from s3...')
+    log.info('Fetching ddebs.json from s3...')
     get_from_s3_compressed(bucket, 'ddebs.json', '/tmp/ddebs.json')
-    scrape_all_ddebs(4)
+    scrape_all_ddebs(4, 'http://ddebs.ubuntu.com/pool/main/')
+    scrape_all_ddebs(4, 'http://us.archive.ubuntu.com/ubuntu/pool/main/',
+                     is_dbg_package)
     put_to_s3_compressed(bucket, 'ddebs.json', '/tmp/ddebs.json')
 
 
