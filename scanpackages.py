b29f7feddbe4c2b40306812626dd409d4cac124e
Ted Mielczarek <ted@mielczarek.org>
2015-05-26 15:23:48 -0400

    allow scraping -dbg packages too

--- scanpackages.py
+++ scanpackages.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 import html5lib
 import itertools
 import json
+import logging
 import multiprocessing
 import os
 import re
@@ -29,6 +30,8 @@ import urlparse
 
 from common import fetch_to_file
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+log = logging.getLogger('scanpackages')
 
 class AutoSaveDict(dict):
     def __init__(self, path):
@@ -64,50 +67,8 @@ file, and if so to try to get its build ID. If no build ID is found then it retu
                 return buildID
     return None
 
-
-
-def FillPackageList(data):
-    """This function reads the specified file, which is assumed to be a Packages
-file such as those found at http://ddebs.ubuntu.com/dists/precise/main/binary-i386/Packages
-and breaks it into individual package description blocks. These blocks are then put into
-a dictionary, indexed by the download URL."""
-    # Individual package descriptions start with a line that starts with Package: so splitting
-    # on this is a simple way to break the file into package blocks.
-    packageLabel = "Package: "
-    packageCount = 0
-    # The URL is the only part of the package that we parse. It is contained in a line
-    # that starts with 'Filename: '
-    filenameRe = re.compile("Filename: (.*)")
-    result = {}
-    try:
-        for block in data.split("\n" + packageLabel):
-            # The splitting process removes the package label from
-            # the beginning of all but the first block so let's put it
-            # back on.
-            if not block.startswith( packageLabel ):
-                block = packageLabel + block
-            # Look for the package URL
-            for line in block.split("\n"):
-                line = line.strip()
-                match = filenameRe.match(line)
-                if match:
-                    packageURL = match.groups()[0]
-                    # For some reason the Packages file lists some packages multiple times with
-                    # the exact same download URL. In every case seen so far the package description
-                    # is identical, but lets print a message if that stops being true.
-                    if result.has_key(packageURL) and block.strip() != result[packageURL].strip():
-                        print("Download URL %s found multiple times with different descriptions." % packageURL)
-                    packageCount += 1
-                    result[packageURL] = block
-    except IOError as e:
-        # On the first run the PackagesProcessed file will not exist. We must continue.
-        print(e)
-    print("Found %d packages" % (packageCount))
-    return result
-
-
 def process_deb(deb_url):
-    print('Processing deb %s...' % deb_url)
+    log.info('Processing deb %s...' % deb_url)
     buildid_files = []
     try:
         tempDir = tempfile.mkdtemp()
@@ -121,6 +82,9 @@ def process_deb(deb_url):
         for root, dirs, files in os.walk(tempDir):
             for f in files:
                 path = os.path.join(root, f)
+                # There are dangling symlinks in -dbg packages...
+                if not os.path.exists(f):
+                    continue
                 buildID = GetBuildID(path)
                 if buildID:
                     buildid_files.append(('/' + os.path.relpath(path, tempDir),
@@ -147,14 +111,15 @@ def scrape_x86_debs(url):
             yield deb
 
 def scrape_package_list(main_url):
-    if os.path.isfile('/tmp/allpackages'):
-        return json.load(open('/tmp/allpackages', 'rb'))
+    cached_allpackages = '/tmp/%s_allpackages' % ''.join(x for x in main_url if x.isalnum())
+    if os.path.isfile(cached_allpackages):
+        return json.load(open(cached_allpackages, 'rb'))
 
-    print('Scraping package listing...')
+    log.info('Scraping package listing from %s...' % main_url)
     package_list = []
     for url in scrape_html_directory_listing(main_url):
         package_list.extend(list(scrape_html_directory_listing(url)))
-        json.dump(package_list, open('/tmp/allpackages', 'wb'))
+        json.dump(package_list, open(cached_allpackages, 'wb'))
     return package_list
 
 def chunk(iterable, chunk_size):
@@ -165,32 +130,45 @@ def chunk(iterable, chunk_size):
             return
         yield this_chunk
 
-def scrape_all_ddebs(worker_count):
+def scrape_all_ddebs(worker_count, main_url, filter_func=None):
     ddebs = AutoSaveDict('/tmp/ddebs.json')
     processed_packages = AutoSaveDict('/tmp/processed-packages.json')
     skip_packages = {}
     with ThreadPoolExecutor(max_workers=worker_count) as executor:
-        package_urls = [url for url in scrape_package_list('http://ddebs.ubuntu.com/pool/main/') if url not in processed_packages]
+        package_urls = [url for url in scrape_package_list(main_url) if url not in processed_packages]
         for urls_chunk in chunk(package_urls, worker_count):
-            print('Processing next %d packages...' % len(urls_chunk))
+            log.info('Processing next %d packages...' % len(urls_chunk))
             for url, debs in zip(urls_chunk,
                                  executor.map(scrape_x86_debs, urls_chunk)):
-                print('Processing package %s...' % url)
-                debs = [deb for deb in debs if deb not in ddebs and not os.path.basename(urlparse.urlparse(deb).path).startswith('linux-image')]
-                print('%d debs to process...' % len(debs))
+                log.info('Processing package %s...' % url)
+                debs = [
+                    deb for deb in debs if deb not in ddebs
+                    # The linux-image packages are just huge.
+                    and not os.path.basename(urlparse.urlparse(deb).path).startswith('linux-image')
+                    and (filter_func(deb) if filter_func else True)
+                ]
+                log.info('%d debs to process...' % len(debs))
                 deb_jobs = dict((executor.submit(process_deb, deb), deb) for deb in debs)
                 for future in as_completed(deb_jobs):
                     deb = deb_jobs[future]
                     if future.exception() is not None:
-                        print('Error processing %s: %s' % (deb, future.exception()))
+                        log.info('Error processing %s: %s' % (deb, future.exception()))
                     else:
-                        print('Finished processing deb %s' % deb)
+                        log.info('Finished processing deb %s' % deb)
                         ddebs[deb] = future.result()
                 processed_packages[url] = True
 
+def is_dbg_package(url):
+    name = os.path.splitext(os.path.basename(urlparse.urlparse(url).path))[0].split('_')[0]
+    return name.endswith('-dbg')
+
 def main():
+    logging.basicConfig(filename='scanpackages.log',
+                        level=logging.DEBUG)
     n = int(sys.argv[1]) if len(sys.argv) > 1 else multiprocessing.cpu_count()
-    scrape_all_ddebs(n)
+    scrape_all_ddebs(n, 'http://ddebs.ubuntu.com/pool/main/')
+    scrape_all_ddebs(n, 'http://us.archive.ubuntu.com/ubuntu/pool/main/',
+                     is_dbg_package)
 
 if __name__ == '__main__':
     main()
